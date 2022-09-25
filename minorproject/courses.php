041c86fd0c31c254918404e05254f1e077b0e105
Suman Adhikari <58351346+thesuman74@users.noreply.github.com>
2022-09-25 17:07:27 +0545

    Add files via upload

---
+++ minorproject/courses.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="format-detection" content="telephone=no">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+
+
+	<title>courses</title>
+</head>
+<body>
+
+	<!-- <header -->
+
+		<?php
+		include 'includes/header.php';
+		include 'includes/scripts.php';
+		?>
+		<!-- header ends here -->
+
+		<!-- section starts here  -->
+		<section id="popular-books" class="bookshelf">
+			<div class="container">
+				<div class="row">
+					<div class="inner-content">
+
+						<div class="section-header align-center">
+							<div class="title">
+								<span>Our courses</span>
+							</div>
+							<h2 class="section-title">Popular Courses</h2>
+						</div>
+
+					
+
+						<div class="tab-content">
+								<div id="all-genre" data-tab-content class="active" data-aos="fade-up">
+									<div class="grid">
+
+										
+
+										<figure class="product-style">
+											<img src="images/tab-item7.jpg" alt="Books" class="product-item ml-4">
+											<figcaption>
+												<h3>Computer Engineering</h3>
+											</figcaption>
+										</figure>
+
+										
+
+										<figure class="product-style">
+											<a href="software/semesters.php"><img src="images/tab-item8.jpg" alt="Books" class="product-item mr-4"></a>
+											<figcaption>
+												<h3>Software Engineering</h3>
+											</figcaption>
+										</figure>
+
+									</div>
+
+								</div>
+								
+
+								
+
+								
+
+							</div>
+
+					</div><!--inner-tabs-->
+
+				</div>
+			</div>
+		</section>
+
+		<?php
+include 'includes/footer.php';
+		?>
+
+
+
+
+
+	</body>
+	</html>
---
+++ minorproject/software/subjects.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>First_sem_subjects</title>
+
+	<link rel="stylesheet" type="text/css" href="../style.css">
+
+	<style >
+		a{
+			align-items: center;
+			line-height: 28px;
+			justify-content: center;
+
+		}
+	</style>
+
+</head>
+<body>
+
+	<!-- <header -->
+
+		<?php
+			include '../includes/header.php';
+			include '../includes/scripts.php';
+			include '../links/links.php';
+		?>
+		<!-- header ends here -->
+
+		<!-- section starts here  -->
+		<section id="popular-books" class="bookshelf">
+			<div class="container">
+				<div class="row">
+					<div class="inner-content">
+
+						<div class="section-header align-center">
+							<div class="title">
+								<div class="row float-left">
+									<span class="text-danger "><a class="text-secondary " href="../courses.php">courses</a>>><a class="text-secondary " href="semesters.php">Semesters</a>>><a class="text-secondary " href="subjects.php">Subjects</a></span>
+									
+								</div><br>
+
+							</div>
+							<h2 class="section-title " style="z-index: 10; color: gray;" >Engineering Mathematics</h2>
+						</div>	
+
+						<div class="container">
+									<span>From 2013-2022</span><br>
+							<div class="row">
+								<div class="col-sm ">
+									<iframe class="shadow-lg p-3 mb-5 bg-white rounded"  src="https://drive.google.com/file/d/1_fNfCvL73wYLLpllFMjQoX8XJIoknhxg/preview" width="400" height="300" allow=""></iframe>
+								</div>
+															<div class="col-sm">
+									<a class="btn btn-success btn-lg d-flex  text-white" style="width:200px; height: 30px;" > Download </a>
+								</div>
+								
+							</div>
+
+						</div>
+
+
+					</div>
+				</section>
+
+
+
+
+
+
+
+
+
+
+			</body>
+			</html>
